139fb02deee6d57045b89212cbad268a0c8f13b8
Fcott <f.f.f.kt13@gmail.com>
2016-12-22 15:26:21 +0900

    added new features and config

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -14,4 +14,10 @@ class MessageTest < ActiveSupport::TestCase
     @message.content = " "
     assert !@message.valid?
   end
+
+  test "mentions" do
+    @message.content = "This mentions @example and @nonexistent"
+    assert @message.mentions.include?(users(:example))
+    # assert_not @message.mentions.include?(users(:nonexistent))
+  end
 end
